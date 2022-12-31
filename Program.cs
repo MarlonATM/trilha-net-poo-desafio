f682b1481196b1652edd705a07e1dc6c999088dc
MarlonATM <marlon-am@hotmail.com>
2022-12-31 17:09:17 +0000

    Subindo alterações de desafio concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("999454222", "N90", "74OD45", 8);
+Console.WriteLine($"Smartphone número {nokia.Numero}:");
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.Write("\n");
+
+Smartphone iphone = new Iphone("999727299", "14 PRO", "26UBFD98", 8);
+Console.WriteLine($"Smartphone número {iphone.Numero}: ");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("COD");
